d13deee622dc1de256c456618c4f24e4ffb04f6a
Trevor Sullivan <trevor@balena.io>
2019-02-26 15:55:10 -0800

    Adding Find-WiFiNetwork function

---
+++ Functions/Public/Find-WifiNetwork.ps1
@@ -0,0 +1,24 @@
+function Find-WifiNetwork {
+    [CmdletBinding()]
+    param ()
+
+    Disconnect-WiFi
+    $Command = 'netsh wlan show networks'
+    $Result = Invoke-Expression -Command $Command
+
+    #($Result -join '') -match '(?<result>SSID.*?)(?=SSID)'
+    $NetworkRegex = '(?<=: )(?<SSID>.*?)\W+Network.*?: (?<Type>.*?)\W.*: (?<Authentication>.*?)\W.*?: (?<Encryption>.*?)\W'
+    $Regex = [regex]'(?<result>SSID.*?)(?=SSID)'
+    $MatchList = $Regex.Matches(($Result -join ''))
+    foreach ($Match in $MatchList) {
+        Write-Debug -Message $match.Value
+        if ($Match.Value -match $NetworkRegex) {
+            [PSCustomObject]@{
+                SSID = $matches.SSID
+                Encryption = $matches.Encryption
+                Authentication = $matches.Authentication
+                NetworkType = $matches.Type
+            }
+        }
+    }
+}
