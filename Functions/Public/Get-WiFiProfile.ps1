2a058741b65f311e15fd041b37e478f8100147c7
Trevor Sullivan <trevor@balena.io>
2019-02-26 15:54:29 -0800

    Adding command to retrieve WiFi profiles

---
+++ Functions/Public/Get-WiFiProfile.ps1
@@ -0,0 +1,15 @@
+function Get-WiFiProfile {
+    <#
+    .SYNOPSIS
+    Retrieves a list of configured WiFi profiles.
+    #>
+    [CmdletBinding()]
+    param ()
+
+    $ProfileList = Invoke-Expression -Command 'netsh wlan show profile'
+    $ProfileList | ForEach-Object -Process {
+        $matches = $null
+        $null = $PSItem -match ': (?<ProfileName>.*)$'
+        $matches.ProfileName
+    }
+}
