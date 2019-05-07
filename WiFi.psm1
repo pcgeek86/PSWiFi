cd621a689f986563bd467a9dcd9a829b3298b972
Trevor Sullivan <trevor@balena.io>
2019-05-07 10:31:44 -0700

    Add Intellisense tab-completion functions

--- WiFi.psm1
+++ WiFi.psm1
@@ -1,10 +1,14 @@
-$PrivateFunctionList = Get-ChildItem -Path $PSScriptRoot/Functions/Private
+$PrivateFunctionList = Get-ChildItem -Path $PSScriptRoot/Functions/Private -Filter *.ps1
 foreach ($File in $PrivateFunctionList) {
     . $File.FullName
 }
 
-$PublicFunctionList = Get-ChildItem -Path $PSScriptRoot/Functions/Public
+$PublicFunctionList = Get-ChildItem -Path $PSScriptRoot/Functions/Public -Filter *.ps1
 foreach ($File in $PublicFunctionList) {
     . $File.FullName
 }
 
+$CompleterList = Get-ChildItem -Path $PSScriptRoot/Completers -Filter *.ps1
+foreach ($File in $CompleterList) {
+    . $File.FullName
+}
