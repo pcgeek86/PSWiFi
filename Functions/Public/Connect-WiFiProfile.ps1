788574555e6163027ef2a3d75120ad22c7f89643
Trevor Sullivan <trevor@balena.io>
2019-05-07 10:29:56 -0700

    Added in-line help to Connect-WiFiProfile

--- Functions/Public/Connect-WiFiProfile.ps1
+++ Functions/Public/Connect-WiFiProfile.ps1
@@ -1,13 +1,34 @@
 function Connect-WiFiProfile {
+    <#
+    .Synopsis
+    Connects to a configured WiFi connection profile.
+
+    .Parameter Name
+    The name of the WiFi profile that you would like to connect to.
+    Use the Get-WiFiProfile command to discover existing profiles.
+
+    .Parameter SSID
+    Specify the SSID to connect to, within the given WiFi profile.
+    This parameter is mandatory if the WiFi profile, specified in the -Name parameter, contains multiple SSIDs.
+
+    .Parameter Interface
+    The network interface that you would like to connect to the WiFi profile.
+
+    .Example
+    Connect to an existing WiFi profile:
+
+      Connect-WiFiProfile -Name MyProfileName
+
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
-        [string] $ProfileName
+        [string] $Name
       , [string] $SSID
       , [string] $Interface
     )
 
-    $Command = 'netsh wlan connect name="{0}"' -f $ProfileName
+    $Command = 'netsh wlan connect name="{0}"' -f $Name
 
     switch ($true) {
         $SSID {
